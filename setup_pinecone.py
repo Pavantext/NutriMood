7aa4b9cb93c0a212cf75a647e8dfc5166941b0e6
Pavan Kumar M <pavantext6@gmail.com>
2025-06-05 16:51:04 +0530

    changed json name

--- setup_pinecone.py
+++ setup_pinecone.py
@@ -4,7 +4,7 @@ from utils.embeddings import get_embedding
 from utils.pinecone_helper import get_new_index, upsert_data
 
 # Load your JSON food data
-with open("data/food_items.json", "r") as f:
+with open("data/niloufer.json", "r") as f:
     food_data = json.load(f)
 
 index = get_new_index()
